5697fa8bff67e8a3f93660c16261419f1eb6e1f1
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2019-01-07 17:17:00 +0300

    Feature: Add a service for the different twitter interactions, sending tweets, viewing timelines and sending direct messages

--- src/main/java/com/gaamuwa/twitter/services/TwitterServiceImpl.java
+++ src/main/java/com/gaamuwa/twitter/services/TwitterServiceImpl.java
@@ -1,20 +1,68 @@
 package com.gaamuwa.twitter.services;
 
-import org.springframework.social.twitter.api.Tweet;
-import org.springframework.social.twitter.api.Twitter;
-import org.springframework.social.twitter.api.TwitterProfile;
-import org.springframework.social.twitter.api.impl.TwitterTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import twitter4j.*;
 
+import java.nio.file.Path;
 import java.util.List;
+
 
 @Service
 public class TwitterServiceImpl implements TwitterService {
 
-    Twitter twitter = new TwitterTemplate("", "", "", "");
+    @Autowired
+    private Twitter twitter;
 
+    @Override
     public void sendTweet(String status){
-        Tweet tweet = twitter.timelineOperations().updateStatus(status);
-        System.out.println(tweet.toString());
+        try{
+            Status returnedStatus  = twitter.updateStatus(status);
+            System.out.println("Status updated: " + returnedStatus.getText());
+            System.out.println("♥ : " + returnedStatus.getFavoriteCount());
+        }
+        catch (TwitterException exception){
+            System.out.println("There was a twitter exception");
+            System.out.println(exception.getErrorMessage());
+        }
     }
+
+    @Override
+    public void sendTweet(String status, Path mediaPath){
+        StatusUpdate statusUpdate = new StatusUpdate(status);
+        try{
+            statusUpdate.setMedia(mediaPath.toFile());
+            Status returnedStatus = twitter.updateStatus(statusUpdate);
+            System.out.println("Status Updated: "+ returnedStatus.getText());
+        }catch (TwitterException exception){
+            System.out.println("There was a twitter exception");
+        }
+    }
+
+    @Override
+    public void sendDirectMessage(String twitterHandle, String message) {
+        try{
+            DirectMessage directMessage = twitter.sendDirectMessage(twitterHandle, message);
+        }catch (TwitterException exception){
+            System.out.println("Failure sending direct message to: " + twitterHandle );
+            System.out.println("Error message: "+ exception.getMessage());
+        }
+    }
+
+    @Override
+    public void getDefaultTimeline() {
+        try{
+            List<Status> statuses = twitter.getHomeTimeline();
+            for (Status status : statuses){
+                System.out.println(status.getText());
+                System.out.println("By: @"+status.getUser().getScreenName());
+                System.out.println();
+                System.out.println("---------------------------------------------------");
+            }
+        }catch (TwitterException exception){
+            System.out.println("Failure getting timeline: " + exception.getMessage());
+        }
+    }
+
+
 }
