a6c28b6831dbd2182aca3f16d545a12a902ed207
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2019-01-07 17:17:51 +0300

    Task: Write nested classes for the different twitter commands

--- src/main/java/com/gaamuwa/twitter/TwitterCommands.java
+++ src/main/java/com/gaamuwa/twitter/TwitterCommands.java
@@ -1,28 +1,50 @@
 package com.gaamuwa.twitter;
 
 import com.gaamuwa.twitter.services.TwitterService;
+import com.kakawait.spring.boot.picocli.autoconfigure.ExitStatus;
 import com.kakawait.spring.boot.picocli.autoconfigure.HelpAwarePicocliCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import picocli.CommandLine;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 
 @Component
-@Command(name = "twitter")
-public class TwitterCommands extends HelpAwarePicocliCommand {
+public class TwitterCommands {
 
     @Autowired
     private TwitterService twitterService;
 
 
-    @Parameters(paramLabel = "STATUS", description = "status")
-    String status;
+    @Component
+    @Command(name = "status")
+    public class StatusCommand extends HelpAwarePicocliCommand {
+        @CommandLine.Option(
+                names = {"-n", "--new"}
+        )
+        boolean newStatus;
 
-    @Override
-    public void run(){
-        if(!status.isEmpty()){
-            twitterService.sendTweet(status);
-            System.out.println(status);
+        @Parameters(paramLabel = "STATUS", description = "status")
+        String status;
+
+        @Override
+        public void run(){
+            if(!status.isEmpty() && newStatus){
+                twitterService.sendTweet(status);
+                System.out.println(status);
+            }
         }
     }
+
+    @Component
+    @Command(name = "timeline")
+    public class TimelineCommand extends HelpAwarePicocliCommand {
+
+        @Override
+        public void run(){
+            twitterService.getDefaultTimeline();
+        }
+
+    }
+
 }
