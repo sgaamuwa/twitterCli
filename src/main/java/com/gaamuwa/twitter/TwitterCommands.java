d75159656ca3c3cd0d4ca12c6999911af02fc9dc
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2019-01-10 14:54:19 +0300

    Feature: Add commands for accessing the timeline and direct messages

--- src/main/java/com/gaamuwa/twitter/TwitterCommands.java
+++ src/main/java/com/gaamuwa/twitter/TwitterCommands.java
@@ -5,7 +5,7 @@ import com.kakawait.spring.boot.picocli.autoconfigure.ExitStatus;
 import com.kakawait.spring.boot.picocli.autoconfigure.HelpAwarePicocliCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import picocli.CommandLine;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 
@@ -19,17 +19,20 @@ public class TwitterCommands {
     @Component
     @Command(name = "status")
     public class StatusCommand extends HelpAwarePicocliCommand {
-        @CommandLine.Option(
+        // user can specify that it is a new status
+        @Option(
                 names = {"-n", "--new"}
         )
         boolean newStatus;
 
+        // status provided by the user for the new status
         @Parameters(paramLabel = "STATUS", description = "status")
         String status;
 
         @Override
         public void run(){
-            if(!status.isEmpty() && newStatus){
+            // user should have specified that it is a new status and it shouldn't be empty
+            if(!status.trim().isEmpty() && newStatus){
                 twitterService.sendTweet(status);
                 System.out.println(status);
             }
@@ -39,12 +42,38 @@ public class TwitterCommands {
     @Component
     @Command(name = "timeline")
     public class TimelineCommand extends HelpAwarePicocliCommand {
+        // user can specify that they want to see a particular user's timeline
+        @Option(names = {"-u", "--user"})
+        boolean user;
+
+        @Parameters(paramLabel = "SCREENNAME", description = "screenname for the person whose timeline were are retrieving", defaultValue = "user")
+        String screenName;
 
         @Override
         public void run(){
-            twitterService.getDefaultTimeline();
+            if(user && !screenName.trim().isEmpty()){
+                twitterService.getUserTimeline(screenName);
+            }
+            // if the user option is not provided then get the default timeline
+            else if(!user){
+                twitterService.getDefaultTimeline();
+            }
         }
+    }
 
+    @Component
+    @Command(name = "dm")
+    public class DirectMessageCommand extends HelpAwarePicocliCommand {
+
+        @Option(names = {"-v", "--view"})
+        boolean view;
+
+        @Override
+        public void run(){
+            if(view){
+                twitterService.viewDirectMessages();
+            }
+        }
     }
 
 }
